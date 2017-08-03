b5d0fb3a68e29cc286bc281c96894233b640cd29
Kenan Erdogan <Kenan.Erdogan@gesis.org>
2017-08-03 18:07:49 +0200

    v1.0.0-beta.3

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0-beta.2',
+    version='1.0.0-beta.3',
 
     description='An algorithm to create an on-demand color-markup of the original authors of the text of any article '
                 'on the (english) Wikipedia.',
